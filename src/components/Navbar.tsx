5c900755e6654f41e5541fd27faf818693b7ef37
Himanshu Jangid <iamahalofan@gmail.com>
2021-09-06 14:06:38 +0530

    button loader

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,13 @@ import './Navbar.css'
 
 export default function Navbar() {
 
+    const [loading, setloading] = useState(true);
+
     return <div className="navbar">
         <div className="sec">
             <div className="left">Get Users</div>
             <div className="right">
-                <button>Get Users</button>
+                <button disabled={loading}>{loading ? <div className="loader"></div> : "Load"}</button>
             </div>
         </div>
     </div>
