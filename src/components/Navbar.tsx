e229b4688e21f42afb5d8665d398f0071e2ffe71
Himanshu Jangid <iamahalofan@gmail.com>
2021-09-06 13:56:50 +0530

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+
+
+
+export default function Navbar() {
+
+    return <div className="navbar">
+        <div className="sec">
+            <div className="left">Get Users</div>
+            <div className="right">
+                <button>Get Users</button>
+            </div>
+        </div>
+    </div>
+}
