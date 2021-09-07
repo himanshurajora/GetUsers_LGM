c97f07d9f3a6086cf1ea55136913d315d31b0f48
Himanshu Jangid <iamahalofan@gmail.com>
2021-09-07 11:04:06 +0530

    button click handler

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,27 @@ import './Navbar.css'
 
 
 
-export default function Navbar() {
+export default function Navbar(props: any) {
 
-    const [loading, setloading] = useState(true);
+    const [loading, setloading] = useState(false)
+
+    const storeData = async () => {
+        setloading(true)
+
+        var res = await fetch("https://reqres.in/api/users?page=1")
+        var data  = await res.json()
+        console.log(data)
+        props.setdata(data)
+        setloading(false)
+    }
 
     return <div className="navbar">
         <div className="sec">
             <div className="left">Get Users</div>
             <div className="right">
-                <button disabled={loading}>{loading ? <div className="loader"></div> : "Load"}</button>
+                <button disabled={loading} onClick={() => {
+                  storeData()
+                }}>{loading ? <div className="loader"></div> : "Load"}</button>
             </div>
         </div>
     </div>
